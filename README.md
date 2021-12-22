aa5fc24b0dcfb794306aeb142b457581f79e4f96
Marco Silva <marcosilva2203@outlook.pt>
2021-12-22 20:50:45 +0000

    Initial Commit
    The beginning of the project.

---
+++ README.md
@@ -0,0 +1,3 @@
+# CollabGameDev
+
+Developed with Unreal Engine 4
---
+++ Source/CollabGameDev/CollabGameDevGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CollabGameDevGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACollabGameDevGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACollabGameDevGameMode();
+};
+
+
+
